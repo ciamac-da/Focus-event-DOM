0a321b241ed1aa3b59ab8f122b5d5e1a26680e42
cia-from-earth <53003092+ciamac-da@users.noreply.github.com>
2019-11-24 11:08:50 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var x = document.getElementById("demo");
+var y = document.getElementById("test");
+function myFunction(){
+//**x.value = x.value.toUpperCase();*/
+//**y.innerHTML = x.value.toUpperCase();*/
+x.style.backgroundColor ="yellow";
+x.style.border = "1px black solid";
+}
